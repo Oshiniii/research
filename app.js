fa51af5d198d4718880009c98544c026d1f628fa
Oshini <IT17039178@my.sliit.lk>
2020-12-03 19:44:22 +0530

    commit

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require('express')
-const {google} = require('googleapis')
+const { google } = require('googleapis')
 const OAuth2Data = require("./credentials.json")
 const multer = require("multer")
 const fs = require("fs")
 const { script } = require('googleapis/build/src/apis/script')
+
+
 
 const app = express()
 
 const CLIENT_ID = OAuth2Data.web.client_id
 const CLIENT_SECRET = OAuth2Data.web.client_secret
 const REDIRECT_URI = OAuth2Data.web.redirect_uris[0]
- 
+
 const oAuth2Client = new google.auth.OAuth2(
     CLIENT_ID,
     CLIENT_SECRET,
@@ -25,31 +27,31 @@ const SCOPES = "https://www.googleapis.com/auth/userinfo.profile"
 app.set("view engine", "ejs")
 
 app.get("/", (req, res) => {
-    if(!authed) {
+    if (!authed) {
         var url = oAuth2Client.generateAuthUrl({
             access_type: 'offline',
             scope: SCOPES
         })
 
         console.log(url)
-        res.render("index", {url:url})
+        res.render("index", { url: url })
     }
-    else{
+    else {
         var oauth2 = google.oauth2({
             auth: oAuth2Client,
             version: 'v2'
         })
 
         //user info
-        oauth2.userinfo.get(function(err, response){
-            if(err) throw err
+        oauth2.userinfo.get(function (err, response) {
+            if (err) throw err
 
             console.log(response.data)
 
             name = response.data.name
             photo = response.data.picture
 
-            res.render("success", {name:name, photo:photo, success:false})
+            res.render("success", { name: name, photo: photo, success: false })
         })
     }
 })
@@ -57,15 +59,15 @@ app.get("/", (req, res) => {
 app.get('/google/callback', (req, res) => {
     const code = req.query.code
 
-    if(code){
+    if (code) {
 
         //get an access token
-        oAuth2Client.getToken(code, function(err, tokens){
-            if(err){
+        oAuth2Client.getToken(code, function (err, tokens) {
+            if (err) {
                 console.log("Error in Authenticaticating")
                 console.log(err)
             }
-            else{
+            else {
                 console.log("Succefully Authenticated")
                 console.log(tokens)
                 oAuth2Client.setCredentials(tokens)
---
+++ views/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Facebook from './views/Facebook';
+
+function App() {
+  return (
+    <div className="App">
+        <p>
+          Facebook Authentication
+        </p>
+        <Facebook />
+    </div>
+  );
+}
+
+export default App;
